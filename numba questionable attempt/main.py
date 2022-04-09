388a1b76e81ab9912714dae31680bd59902c152d
lindy2076 <kosolapov.agent@yandex.ru>
2022-04-09 23:56:21 +0300

    significant improvement

--- numba questionable attempt/main.py
+++ numba questionable attempt/main.py
@@ -6,8 +6,8 @@ def main():
     t = time.time()
     k = 3
     print("This program counts associative operations on all " + str(k) + "-len sets.")
-    # assoc_ops = associative.count(k)
-    # print("default python res: " + str(assoc_ops) + " associative operations. T/me:" + str(time.time() - t))
+    assoc_ops = associative.count(k)
+    print("default python res: " + str(assoc_ops) + " associative operations. T/me:" + str(time.time() - t))
     t = time.time()
     assoc_ops = associative_jit.count(k)
     print("numba res: " + str(assoc_ops) + " associative operations. Time:" + str(time.time() - t))
